9bff0502cb9f7b8e66346eb64c54bbe90a443860
L1337jr <Luisdaniel1337jr@gmail.com>
2022-12-11 16:57:20 -0500

    Edited app.js and index.html

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
-function getComputerChoice()
+console.log("Hello how are you let's play Rock Paper Scissors.")
+let palyerScore = 0
+let computerScore = 0
 
-function playRound(playerSelection, computerSelection) {
-    
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+      case 0:
+        return 'ROCK'
+      case 1:
+        return 'PAPER'
+      case 2:
+        return 'SCISSORS'
+    }
+  }
+
+function playRound (){
+   
+    let playerSelection = prompt("Choose!").toUpperCase()
+    let computerSelection = getComputerChoice()
+    console.log("You pick " + playerSelection + " and the pc picks " + computerSelection)
+
+    if (playerSelection == computerSelection) {
+        console.log("its a tie") 
+    }
+    else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" || 
+    playerSelection === "PAPER" && computerSelection === "ROCK" || 
+    playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        palyerScore++
+        console.log("You Won") 
+    }
+    else if (playerSelection === "SCISSORS" && computerSelection === "ROCK" || 
+    playerSelection === "ROCK" && computerSelection === "PAPER" || 
+    playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+        computerScore++
+        console.log("You lost") 
+    }
+    else {
+         console.log("You need to pick Rock, Paper or Scissors")
+    }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++){
+        playRound()
+    }
+   if (computerScore > palyerScore) {
+    console.log("You lost this game")
+   } else if (palyerScore > computerScore) {
+    console.log("You won this game")
+   } else if (palyerScore == computerScore) {
+    console.log("Tie!")
+   } else {
+    console.log("Something went wrong")
+   }
+}
 
+game()
