fe56bf369a8ce449693f335d89222d9d9aa7e53f
L1337jr <luisdaniel1337jr@gmail.com>
2023-04-07 17:34:09 -0400

    new file:   TwitterColorEmoji-SVGinOT.ttf 	modified:   app.js 	new file:   cool-background.svg 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,79 +1,93 @@
-let palyerScore = 0
-let computerScore = 0
+let palyerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
-      case 0:
-        return 'Rock'
-      case 1:
-        return 'Paper'
-      case 2:
-        return 'Scissors'
-    }
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "✊";
+    case 1:
+      return "✋";
+    case 2:
+      return "✌";
   }
-
-  let rockButton = document.querySelector('#rock');
-  let paperButton = document.querySelector('#paper');
-  let scissorsButton = document.querySelector('#scissors');
-  let scoreplayer = document.querySelector('#playerScore')
-  let scorepc = document.querySelector('#computerScore')
-  let showResult = document.querySelector('#tellWinner')
-
-  rockButton.addEventListener('click', function() {
-    playRound('Rock', computerPlay());
-  });
-  
-  paperButton.addEventListener('click', function() {
-    playRound('Paper', computerPlay());
-  });
-  
-  scissorsButton.addEventListener('click', function() {
-    playRound('Scissors', computerPlay());
-  });
-
-
-
-
-function playRound (playerSelection, computerSelection){
-  explain.textContent = `You pick ${playerSelection}  and the PC picks ${computerSelection}`
-  
-  
-
-    if (playerSelection == computerSelection) {
-      result.textContent = "Its a tie"
-      showResult.innerHTML = ` `
-    }
-    else if (playerSelection === "Rock" && computerSelection === "Scissors" || 
-    playerSelection === "Paper" && computerSelection === "Rock" || 
-    playerSelection === "Scissors" && computerSelection === "Paper") {
-        palyerScore++
-        scoreplayer.innerHTML = `Player Score: ${palyerScore}`
-        result.textContent = "You Won!" 
-        showResult.innerHTML = ` `
-
-    }
-    else{
-        computerScore++
-        result.textContent = "You lost!" 
-        scorepc.innerHTML = `PC Score: ${computerScore}`
-        showResult.innerHTML = ` `
-    }
-    
-    if (palyerScore >= 5) {
-      palyerScore = 0
-      computerScore= 0
-      scoreplayer.innerHTML = `Player Score: ${palyerScore}`
-      scorepc.innerHTML = `PC Score: ${computerScore}`
-      showResult.innerHTML = `You won this round! Congrats`
-    }
-    else if (computerScore >= 5) {
-      palyerScore = 0
-      computerScore= 0
-      scoreplayer.innerHTML = `Player Score: ${palyerScore}`
-      scorepc.innerHTML = `PC Score: ${computerScore}`
-      showResult.innerHTML = `You lost this round! best luck next time.`
-    }
 }
 
+let rockButton = document.querySelector("#rock");
+let paperButton = document.querySelector("#paper");
+let scissorsButton = document.querySelector("#scissors");
+let scoreplayer = document.querySelector("#playerScore");
+let scorepc = document.querySelector("#computerScore");
+let showResult = document.querySelector("#tellWinner");
+let player_result = document.querySelector(".player-choice");
+let pc_result = document.querySelector(".pc-choice");
 
+rockButton.addEventListener("click", function () {
+  playRound("✊", computerPlay());
+  anime();
+});
+
+paperButton.addEventListener("click", function () {
+  playRound("✋", computerPlay());
+  anime();
+});
+
+scissorsButton.addEventListener("click", function () {
+  playRound("✌", computerPlay());
+  anime();
+});
+
+function playRound(playerSelection, computerSelection) {
+  player_result.textContent = `${playerSelection}`;
+  pc_result.textContent = `${computerSelection}`;
+
+  if (playerSelection == computerSelection) {
+    result.textContent = "Its a tie";
+    showResult.innerHTML = ` `;
+  } else if (
+    (playerSelection === "✊" && computerSelection === "✌") ||
+    (playerSelection === "✋" && computerSelection === "✊") ||
+    (playerSelection === "✌" && computerSelection === "✋")
+  ) {
+    palyerScore++;
+    scoreplayer.innerHTML = `Player Score: ${palyerScore}`;
+    result.textContent = "You Won!";
+    showResult.innerHTML = ` `;
+  } else {
+    computerScore++;
+    result.textContent = "You lost!";
+    scorepc.innerHTML = `PC Score: ${computerScore}`;
+    showResult.innerHTML = ` `;
+  }
+
+  if (palyerScore >= 5) {
+    palyerScore = 0;
+    computerScore = 0;
+    scoreplayer.innerHTML = `Player Score: ${palyerScore}`;
+    scorepc.innerHTML = `PC Score: ${computerScore}`;
+    showResult.innerHTML = `You won this round! Congrats`;
+  } else if (computerScore >= 5) {
+    palyerScore = 0;
+    computerScore = 0;
+    scoreplayer.innerHTML = `Player Score: ${palyerScore}`;
+    scorepc.innerHTML = `PC Score: ${computerScore}`;
+    showResult.innerHTML = `You lost this round! best luck next time.`;
+  }
+}
+
+function anime() {
+  if (
+    player_result.classList.contains("animateInLeft") &&
+    pc_result.classList.contains("animateInRigth")
+  ) {
+    player_result.classList.remove("animateInLeft");
+    player_result.classList.add("animateOutLeft");
+    pc_result.classList.remove("animateInRigth");
+    pc_result.classList.add("animateOutRigth");
+  } else {
+    player_result.classList.remove("animateOutLeft");
+    player_result.classList.add("animateInLeft");
+    pc_result.classList.remove("animateOutRigth");
+    pc_result.classList.add("animateInRigth");
+  }
+}
