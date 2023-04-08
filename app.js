f440f87602ce6867b85482abb89e562cdb476638
L1337jr <luisdaniel1337jr@gmail.com>
2023-04-08 17:16:28 -0400

    modified app.js to follow The Module Pattern 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,93 +1,101 @@
-let palyerScore = 0;
-let computerScore = 0;
+const gameModule = (() => {
+  let playerScore = 0;
+  let computerScore = 0;
 
-function computerPlay() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return "✊";
-    case 1:
-      return "✋";
-    case 2:
-      return "✌";
-  }
-}
+  const computerPlay = () => {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return "✊";
+      case 1:
+        return "✋";
+      case 2:
+        return "✌";
+    }
+  };
 
-let rockButton = document.querySelector("#rock");
-let paperButton = document.querySelector("#paper");
-let scissorsButton = document.querySelector("#scissors");
-let scoreplayer = document.querySelector("#playerScore");
-let scorepc = document.querySelector("#computerScore");
-let showResult = document.querySelector("#tellWinner");
-let player_result = document.querySelector(".player-choice");
-let pc_result = document.querySelector(".pc-choice");
+  const playRound = (playerSelection, computerSelection) => {
+    const scoreplayer = document.querySelector("#playerScore");
+    const scorepc = document.querySelector("#computerScore");
+    const showResult = document.querySelector("#tellWinner");
+    let player_result = document.querySelector(".player-choice");
+    let pc_result = document.querySelector(".pc-choice");
 
-rockButton.addEventListener("click", function () {
-  playRound("✊", computerPlay());
-  anime();
-});
+    player_result.textContent = `${playerSelection}`;
+    pc_result.textContent = `${computerSelection}`;
 
-paperButton.addEventListener("click", function () {
-  playRound("✋", computerPlay());
-  anime();
-});
+    if (playerSelection == computerSelection) {
+      showResult.innerHTML = "It's a tie";
+    } else if (
+      (playerSelection === "✊" && computerSelection === "✌") ||
+      (playerSelection === "✋" && computerSelection === "✊") ||
+      (playerSelection === "✌" && computerSelection === "✋")
+    ) {
+      playerScore++;
+      scoreplayer.innerHTML = `Player Score: ${playerScore}`;
+      showResult.innerHTML = "You Won!";
+    } else {
+      computerScore++;
+      scorepc.innerHTML = `PC Score: ${computerScore}`;
+      showResult.innerHTML = "You lost!";
+    }
 
-scissorsButton.addEventListener("click", function () {
-  playRound("✌", computerPlay());
-  anime();
-});
+    if (playerScore >= 5) {
+      playerScore = 0;
+      computerScore = 0;
+      scoreplayer.innerHTML = `Player Score: ${playerScore}`;
+      scorepc.innerHTML = `PC Score: ${computerScore}`;
+      showResult.innerHTML = "You won this round! Congrats";
+    } else if (computerScore >= 5) {
+      playerScore = 0;
+      computerScore = 0;
+      scoreplayer.innerHTML = `Player Score: ${playerScore}`;
+      scorepc.innerHTML = `PC Score: ${computerScore}`;
+      showResult.innerHTML = "You lost this round! best luck next time.";
+    }
 
-function playRound(playerSelection, computerSelection) {
-  player_result.textContent = `${playerSelection}`;
-  pc_result.textContent = `${computerSelection}`;
+    anime(player_result, pc_result);
+  };
 
-  if (playerSelection == computerSelection) {
-    result.textContent = "Its a tie";
-    showResult.innerHTML = ` `;
-  } else if (
-    (playerSelection === "✊" && computerSelection === "✌") ||
-    (playerSelection === "✋" && computerSelection === "✊") ||
-    (playerSelection === "✌" && computerSelection === "✋")
-  ) {
-    palyerScore++;
-    scoreplayer.innerHTML = `Player Score: ${palyerScore}`;
-    result.textContent = "You Won!";
-    showResult.innerHTML = ` `;
-  } else {
-    computerScore++;
-    result.textContent = "You lost!";
-    scorepc.innerHTML = `PC Score: ${computerScore}`;
-    showResult.innerHTML = ` `;
-  }
+  const anime = (player_result, pc_result) => {
+    if (
+      player_result.classList.contains("animateInLeft") &&
+      pc_result.classList.contains("animateInRigth")
+    ) {
+      player_result.classList.remove("animateInLeft");
+      player_result.classList.add("animateOutLeft");
+      pc_result.classList.remove("animateInRigth");
+      pc_result.classList.add("animateOutRigth");
+    } else {
+      player_result.classList.remove("animateOutLeft");
+      player_result.classList.add("animateInLeft");
+      pc_result.classList.remove("animateOutRigth");
+      pc_result.classList.add("animateInRigth");
+    }
+  };
 
-  if (palyerScore >= 5) {
-    palyerScore = 0;
-    computerScore = 0;
-    scoreplayer.innerHTML = `Player Score: ${palyerScore}`;
-    scorepc.innerHTML = `PC Score: ${computerScore}`;
-    showResult.innerHTML = `You won this round! Congrats`;
-  } else if (computerScore >= 5) {
-    palyerScore = 0;
-    computerScore = 0;
-    scoreplayer.innerHTML = `Player Score: ${palyerScore}`;
-    scorepc.innerHTML = `PC Score: ${computerScore}`;
-    showResult.innerHTML = `You lost this round! best luck next time.`;
-  }
-}
+  const listenForClicks = (() => {
+    const buttons = document.querySelectorAll(".btn");
 
-function anime() {
-  if (
-    player_result.classList.contains("animateInLeft") &&
-    pc_result.classList.contains("animateInRigth")
-  ) {
-    player_result.classList.remove("animateInLeft");
-    player_result.classList.add("animateOutLeft");
-    pc_result.classList.remove("animateInRigth");
-    pc_result.classList.add("animateOutRigth");
-  } else {
-    player_result.classList.remove("animateOutLeft");
-    player_result.classList.add("animateInLeft");
-    pc_result.classList.remove("animateOutRigth");
-    pc_result.classList.add("animateInRigth");
-  }
-}
+    buttons.forEach((button) =>
+      button.addEventListener("click", (e) => {
+        if (button.textContent == "✊") {
+          playRound("✊", computerPlay());
+        } else if (button.textContent == "✋") {
+          playRound("✋", computerPlay());
+        } else if (button.textContent == "✌") {
+          playRound("✌", computerPlay());
+        }
+      })
+    );
+  })();
+
+  return {
+    playRound,
+    computerPlay,
+    anime,
+    listenForClicks,
+  };
+})();
+
+gameModule.listenForClicks;
